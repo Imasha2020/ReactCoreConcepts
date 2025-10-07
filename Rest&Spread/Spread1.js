c902302adf34887213da950d5c33baa3195eb0ca
Imasha <imashaudayanganie2020@gmail.com>
2025-10-07 23:22:41 +0530

    spred operator is used for arrays and objects

--- Rest&Spread/Spread1.js
+++ Rest&Spread/Spread1.js
@@ -4,3 +4,12 @@ const numbers2 = [ 6, 7, 8];
 const allNumbers = [...numbers1, ...numbers2];
 
 console.log(allNumbers); 
+
+const obj1 = {first : 10 , second : 20};
+const obj2 = {third : 30 , fourth : 40};
+
+const allValues = {...obj1, ...obj2};
+console.log(allValues);
+
+//[] is used for arrays 
+//{} is used for objects
