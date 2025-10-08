5c72da2a891081cef1729028b3599318687e990d
Imasha <imashaudayanganie2020@gmail.com>
2025-10-08 07:57:54 +0530

    uses of template literals

---
+++ TemplateLiterals/interpolation.js
@@ -0,0 +1,5 @@
+const name = "Imasha";
+const age = 23;
+
+console.log("Hi I am "+ name + "." + " I am " + age + " years Old.");
+console.log(`Hi I am ${name}. I am ${age} years old.`);//backtick is used to define template literals
---
+++ TemplateLiterals/multiLineStrings.js
@@ -0,0 +1,9 @@
+const text1 = "Hello \n\tI \n\t\tam \n\t\t\tImasha";
+
+const text2 = `Hello
+    I 
+        am
+            Tharushi`;
+
+console.log(text1);
+console.log(text2);
