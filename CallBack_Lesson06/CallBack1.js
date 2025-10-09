0dd4add5ef2582fefbacbfcd37d90e65be78eb36
Imasha <imashaudayanganie2020@gmail.com>
2025-10-09 20:16:09 +0530

    CallBack1

---
+++ CallBack_Lesson06/CallBack1.js
@@ -0,0 +1,17 @@
+function openFile(callback){
+    setTimeout(()=>{
+        console.log("File Opened");
+        callback();
+    }, 3000);
+}
+
+function addSomething(){
+    console.log("I am adding something to the file");
+}
+
+function removeSomething(){
+    console.log("I am removing something from the file");
+}
+
+openFile(addSomething);
+openFile(removeSomething);
