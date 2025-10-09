01143aa26474133b3747efb1e30d8fcaa17d63d7
Imasha <imashaudayanganie2020@gmail.com>
2025-10-09 22:48:50 +0530

    promise2

---
+++ Promices_Lesson07/promise2.js
@@ -0,0 +1,55 @@
+function takeShower(){
+    return new Promise((resolve , reject)=>{
+        setTimeout(()=>{
+            const shower = true;
+            if(shower){
+                console.log("I take shower");
+                resolve("I took a shower");
+            }else {
+                reject("I didn't take a shower");
+            }
+            
+        } , 3000);
+    });
+}
+
+function doHomework(){
+    return new Promise((resolve , reject)=>{
+        setTimeout(()=>{
+            const homework = false;
+            if(homework){
+                console.log("I do homework");
+                resolve("I did homework");
+            }else {
+                reject("I didn't do homework");
+            }
+        }, 2000);
+    });
+}
+
+function playGame(){
+    return new Promise((resolve , reject)=>{
+        setTimeout(()=>{
+            const game = true;
+            if(game){
+                console.log("I play game");
+                resolve("I played game");
+            }else {
+                reject("I didn't play game");
+            }
+        }, 1000);
+    });
+}
+
+takeShower().then((result)=>{
+    console.log(result);
+    return doHomework();
+}).then((result)=>{
+    console.log(result);
+    return playGame();
+}).then((result)=>{
+    console.log(result);
+    console.log("All tasks are done");
+}). catch((error)=>{
+    console.log(error);
+})
