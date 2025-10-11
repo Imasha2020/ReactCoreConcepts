20164f39b29ad5b102b3977c8e9e50f6511edef6
Imasha <imashaudayanganie2020@gmail.com>
2025-10-11 14:19:24 +0530

    configured Prittier

---
+++ SimpleApp/src/App.js
@@ -0,0 +1,33 @@
+const Student = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h3", {}, "Amal"),
+    React.createElement("h3", {}, 20),
+    React.createElement("h3", {}, "BSc"),
+  ]);
+};
+
+const StudentCard = () => {
+  return React.createElement(
+    "div",
+    { className: "st-card" },
+    React.createElement("img", {
+      src: "https://static.vecteezy.com/system/resources/previews/048/711/768/non_2x/happy-graduate-student-holding-diploma-and-books-vector.jpg",
+      alt: "Student Image",
+      className: "student-image",
+    }),
+
+    React.createElement(Student),
+  );
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Hello, React!"), //h1 is wrapper element   {} is passed data    "Hello, React!" is content
+    React.createElement(StudentCard),
+    React.createElement(StudentCard),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
