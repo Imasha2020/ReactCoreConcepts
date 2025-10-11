f9aef0a863063cf930a608d3e60729354606a169
Imasha <imashaudayanganie2020@gmail.com>
2025-10-12 01:17:29 +0530

    props

--- SimpleApp/src/App.js
+++ SimpleApp/src/App.js
@@ -1,15 +1,15 @@
 import  React from "react";
 import {createRoot} from "react-dom";
 
-const Student = () => {
+const Student = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h3", {}, "Kamal"),
-    React.createElement("h3", {}, 20),
-    React.createElement("h3", {}, "BSc"),
+    React.createElement("h3", {}, props.name),
+    React.createElement("h3", {}, props.age),
+    React.createElement("h3", {}, props.course),
   ]);
 };
 
-const StudentCard = () => {
+const StudentCard = (props) => {
   return React.createElement(
     "div",
     { className: "st-card" },
@@ -19,15 +19,17 @@ const StudentCard = () => {
       className: "student-image",
     }),
 
-    React.createElement(Student),
+    React.createElement(Student, {...props} , {}),
   );
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Hello, React!"), //h1 is wrapper element   {} is passed data    "Hello, React!" is content
-    React.createElement(StudentCard),
-    React.createElement(StudentCard),
+    React.createElement(StudentCard, {name : "Amal" , age: 20 , course: "BSc"} ,{}),
+    React.createElement(StudentCard, {name : "Kamal" , age: 22 , course: "MSc"} ,{}),
+    React.createElement(StudentCard, {name : "Sonal" , age: 21 , course: "BCom"} ,{}),
+    React.createElement(StudentCard, {name : "Ravi" , age: 23 , course: "BCA"} ,{}),
   ]);
 };
 
